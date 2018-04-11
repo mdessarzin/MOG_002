2eee5494922760b9d756c33b243189c2d94ca58b
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-04-11 14:18:06 +0200

    ok

--- src/pages/actualite/actualite.ts
+++ src/pages/actualite/actualite.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController, ViewController } from 'ionic-angular';
 
+
+
+
+
 @Component({
-  selector: 'page-actualite',
-  templateUrl: 'actualite.html'
+	selector: 'page-actualite',
+	templateUrl: 'actualite.html'
 })
 export class ActualitePage {
 
-  constructor(public navCtrl: NavController, public viewCtrl: ViewController) {
-  }
-  
+	constructor(public navCtrl: NavController, 
+				public viewCtrl: ViewController) {
+	}
+
 }
