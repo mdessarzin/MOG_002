da3d9f345de3b04af9485c61473f8ebc9c75b865
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-04-12 15:10:32 +0200

    s

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -40,7 +40,7 @@ export class AccueilPage {
 		public _player: AudioStreamProvider,
 		public http: Http, 
 		public loadingCtrl: LoadingController,
-		  public musicControls: MusicControls,
+		public musicControls: MusicControls,
 		//private iab: InAppBrowser,
 		 public plt: Platform
 		//private ga: GoogleAnalytics
@@ -79,11 +79,99 @@ export class AccueilPage {
     ionViewDidLoad() {
 		this.loading();
 	}
+settingMusicControl(){
+    this.musicControls.destroy(); // it's the same with or without the destroy 
+    this.musicControls.create({
+      track       : 'Test track',        // optional, default : ''
+      artist      : 'test artist',                       // optional, default : ''
+      cover       : '',      // optional, default : nothing
+      // cover can be a local path (use fullpath 'file:///storage/emulated/...', or only 'my_image.jpg' if my_image.jpg is in the www folder of your app)
+      //           or a remote url ('http://...', 'https://...', 'ftp://...')
+      isPlaying   : true,                         // optional, default : true
+      dismissable : true,                         // optional, default : false
+    
+      // hide previous/next/close buttons:
+      hasPrev   : false,      // show previous button, optional, default: true
+      hasNext   : false,      // show next button, optional, default: true
+      hasClose  : true,       // show close button, optional, default: false
+      hasSkipForward : false,  // show skip forward button, optional, default: false
+      hasSkipBackward : false, // show skip backward button, optional, default: false
+      skipForwardInterval: 15, // display number for skip forward, optional, default: 0
+      skipBackwardInterval: 15, // display number for skip backward, optional, default: 0
+    // iOS only, optional
+      album       : 'test album',     // optional, default: ''
+      duration : 0, // optional, default: 0
+      elapsed : 0, // optional, default: 0
+    
+      // Android only, optional
+      // text displayed in the status bar when the notific\ation (and the ticker) are updated
+      ticker    : 'Now playing test'
+     });
+     this.musicControls.subscribe().subscribe((action) => {
+      console.log('action', action);
+          const message = JSON.parse(action).message;
+          console.log('message', message);
+          switch(message) {
+            case 'music-controls-next':
+               // Do something
+               break;
+            case 'music-controls-previous':
+               // Do something
+               break;
+            case 'music-controls-pause':
+               // Do something
+               console.log('music pause');
+               this.file.pause();
+               this.musicControls.listen(); 
+               this.musicControls.updateIsPlaying(false);
+               break;
+            case 'music-controls-play':
+               // Do something
+               console.log('music play');
+               this.file.play();
+               this.musicControls.listen(); 
+               this.musicControls.updateIsPlaying(true);
+               break;
+            case 'music-controls-destroy':
+               // Do something
+               break;
+            // External controls (iOS only)
+            case 'music-controls-toggle-play-pause' :
+              // Do something
+              break;
+            case 'music-controls-seek-to':
+              // Do something
+              break;
+            case 'music-controls-skip-forward':
+              // Do something
+              break;
+            case 'music-controls-skip-backward':
+              // Do something
+              break;
 
+              // Headset events (Android only)
+              // All media button events are listed below
+            case 'music-controls-media-button' :
+                // Do something
+                break;
+            case 'music-controls-headset-unplugged':
+                // Do something
+                break;
+            case 'music-controls-headset-plugged':
+                // Do something
+                break;
+            default:
+                break;
+          }
+    });
+    this.musicControls.listen(); // activates the observable above
+    this.musicControls.updateIsPlaying(true);
+  }
+
+	
 startAudio() {      
-    if (this.plt.is('cordova')) {
-      alert('oui');
-   
+   // if (this.plt.is('cordova')) {
+     
         if(localStorage.player == 'play'){
                 this._player.pauseProvider();
                 //this._musicControls.updateIsPlaying(true);
@@ -107,115 +195,31 @@ startAudio() {
 			console.log('Play Button clicked');
 			this._player.playProvider();
 
+	    this.settingMusicControl();
+			
+			alert('o');
+			
+
+			
 			
 			
 			
 			
 			
-			this.musicControls.create({
-  track       : 'Time is Running Out',        // optional, default : ''
-  artist      : 'Muse',                       // optional, default : ''
-  cover       : 'albums/absolution.jpg',      // optional, default : nothing
-  // cover can be a local path (use fullpath 'file:///storage/emulated/...', or only 'my_image.jpg' if my_image.jpg is in the www folder of your app)
-  //           or a remote url ('http://...', 'https://...', 'ftp://...')
-  isPlaying   : true,                         // optional, default : true
-  dismissable : true,                         // optional, default : false
-
-  // hide previous/next/close buttons:
-  hasPrev   : false,      // show previous button, optional, default: true
-  hasNext   : false,      // show next button, optional, default: true
-  hasClose  : true,       // show close button, optional, default: false
-
-// iOS only, optional
-  album       : 'Absolution',     // optional, default: ''
-  duration : 60, // optional, default: 0
-  elapsed : 10, // optional, default: 0
-  hasSkipForward : true,  // show skip forward button, optional, default: false
-  hasSkipBackward : true, // show skip backward button, optional, default: false
-  skipForwardInterval: 15, // display number for skip forward, optional, default: 0
-  skipBackwardInterval: 15, // display number for skip backward, optional, default: 0
-  hasScrubbing: false, // enable scrubbing from control center and lockscreen progress bar, optional
-
-  // Android only, optional
-  // text displayed in the status bar when the notification (and the ticker) are updated, optional
-  ticker    : 'Now playing "Time is Running Out"',
-  // All icons default to their built-in android equivalents
- // The supplied drawable name, e.g. 'media_play', is the name of a drawable found under android/res/drawable* folders
-  playIcon: 'media_play',
-  pauseIcon: 'media_pause',
-  prevIcon: 'media_prev',
-  nextIcon: 'media_next',
-  closeIcon: 'media_close',
-  notificationIcon: 'notification'
- });
-
- this.musicControls.subscribe().subscribe(action => {
-
-   function events(action) {
-     const message = JSON.parse(action).message;
-         switch(message) {
-             case 'music-controls-next':
-                 // Do something
-                 break;
-             case 'music-controls-previous':
-                 // Do something
-                 break;
-             case 'music-controls-pause':
-                 // Do something
-                 break;
-             case 'music-controls-play':
-                 // Do something
-                 break;
-             case 'music-controls-destroy':
-                 // Do something
-                 break;
-
-         // External controls (iOS only)
-         case 'music-controls-toggle-play-pause' :
-                 // Do something
-                 break;
-         case 'music-controls-seek-to':
-           const seekToInSeconds = JSON.parse(action).position;
-           this.musicControls.updateElapsed({
-             elapsed: seekToInSeconds,
-             isPlaying: true
-           });
-           // Do something
-           break;
-         case 'music-controls-skip-forward':
-           // Do something
-           break;
-         case 'music-controls-skip-backward':
-           // Do something
-           break;
-
-             // Headset events (Android only)
-             // All media button events are listed below
-             case 'music-controls-media-button' :
-                 // Do something
-                 break;
-             case 'music-controls-headset-unplugged':
-                 // Do something
-                 break;
-             case 'music-controls-headset-plugged':
-                 // Do something
-                 break;
-             default:
-                 break;
-         }
-     }
-
- this.musicControls.listen(); // activates the observable above
-
- this.musicControls.updateIsPlaying(true);
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			
 
-			
-			}
-		
-		 )};
 			}
 	
 }
-}
+// }
+	}
+	
