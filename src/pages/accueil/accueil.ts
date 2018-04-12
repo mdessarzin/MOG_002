3ef0b48b826871a6234b9dd7764c284b60b195b0
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-04-12 14:52:43 +0200

    w

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -40,11 +40,13 @@ export class AccueilPage {
 		public _player: AudioStreamProvider,
 		public http: Http, 
 		public loadingCtrl: LoadingController,
-		  private _musicControls: MusicControls,
+		  public musicControls: MusicControls,
 		//private iab: InAppBrowser,
-		private _PLATFORM : Platform
+		 public plt: Platform
 		//private ga: GoogleAnalytics
 	){
+			
+
 			
 		if(localStorage.player == 'play'){
             this.buttonIcon = "ios-pause";
@@ -76,11 +78,12 @@ export class AccueilPage {
 	//Chargement du flux JSON
     ionViewDidLoad() {
 		this.loading();
-  	
 	}
 
 startAudio() {      
-    
+    if (this.plt.is('cordova')) {
+      alert('oui');
+   
         if(localStorage.player == 'play'){
                 this._player.pauseProvider();
                 //this._musicControls.updateIsPlaying(true);
@@ -96,85 +99,7 @@ startAudio() {
 			
 			});
 			
-			this._musicControls.destroy(); // it's the same with or without the destroy 
-			this._musicControls.create({
-			track       : 'test',        // optional, default : ''
-			artist      : 'test',                       // optional, default : ''
-			cover       : '',      // optional, default : nothing
-			isPlaying   : true,                         // optional, default : true
-			dismissable : true,                         // optional, default : false
-			hasPrev   : false,      // show previous button, optional, default: true
-			hasNext   : false,      // show next button, optional, default: true
-			hasClose  : false,       // show close button, optional, default: false
-			hasSkipForward : false,  // show skip forward button, optional, default: false
-			hasSkipBackward : false, // show skip backward button, optional, default: false
-			skipForwardInterval: 0, // display number for skip forward, optional, default: 0
-			skipBackwardInterval: 0, // display number for skip backward, optional, default: 0
-			album       : 'Radio Cristy Player',     // optional, default: ''
-			duration : 0, // optional, default: 0
-			elapsed : 0, // optional, default: 0
-			ticker    : 'En ce moment'
-			});
-			 this._musicControls.subscribe().subscribe((action) => {
-			console.log('action', action);
-			const message = JSON.parse(action).message;
-			console.log('message', message);
-			switch(message) {
-			case 'music-controls-next':
-			   // Do something
-			   break;
-			case 'music-controls-previous':
-			   // Do something
-			   break;
-			case 'music-controls-pause':
-			   // Do something
-			   console.log('music pause');
-			   this._player.pauseProvider();
-			   this._musicControls.listen(); 
-			   this._musicControls.updateIsPlaying(false);
-			   break;
-			case 'music-controls-play':
-			   // Do something
-			   console.log('music play');
-
-			   this._player.playProvider();
-			   this._musicControls.listen(); 
-			   this._musicControls.updateIsPlaying(true);
-			   break;
-			case 'music-controls-destroy':
-			   // Do something
-			   break;
-			// External controls (iOS only)
-			case 'music-controls-toggle-play-pause' :
-			  // Do something
-			  break;
-			case 'music-controls-seek-to':
-			  // Do something
-			  break;
-			case 'music-controls-skip-forward':
-			  // Do something
-			  break;
-			case 'music-controls-skip-backward':
-			  // Do something
-			  break;
-
-			  // Headset events (Android only)
-			  // All media button events are listed below
-			case 'music-controls-media-button' :
-				// Do something
-				break;
-			case 'music-controls-headset-unplugged':
-				// Do something
-				break;
-			case 'music-controls-headset-plugged':
-				// Do something
-				break;
-			default:
-				break;
-			}
-			});
-			this._musicControls.listen(); // activates the observable above
-			this._musicControls.updateIsPlaying(true);	
+	
 
 			localStorage.setItem("player", "play");
 			this.buttonIcon = "ios-pause";
@@ -182,8 +107,115 @@ startAudio() {
 			console.log('Play Button clicked');
 			this._player.playProvider();
 
+			
+			
+			
+			
+			
+			this.musicControls.create({
+  track       : 'Time is Running Out',        // optional, default : ''
+  artist      : 'Muse',                       // optional, default : ''
+  cover       : 'albums/absolution.jpg',      // optional, default : nothing
+  // cover can be a local path (use fullpath 'file:///storage/emulated/...', or only 'my_image.jpg' if my_image.jpg is in the www folder of your app)
+  //           or a remote url ('http://...', 'https://...', 'ftp://...')
+  isPlaying   : true,                         // optional, default : true
+  dismissable : true,                         // optional, default : false
+
+  // hide previous/next/close buttons:
+  hasPrev   : false,      // show previous button, optional, default: true
+  hasNext   : false,      // show next button, optional, default: true
+  hasClose  : true,       // show close button, optional, default: false
+
+// iOS only, optional
+  album       : 'Absolution',     // optional, default: ''
+  duration : 60, // optional, default: 0
+  elapsed : 10, // optional, default: 0
+  hasSkipForward : true,  // show skip forward button, optional, default: false
+  hasSkipBackward : true, // show skip backward button, optional, default: false
+  skipForwardInterval: 15, // display number for skip forward, optional, default: 0
+  skipBackwardInterval: 15, // display number for skip backward, optional, default: 0
+  hasScrubbing: false, // enable scrubbing from control center and lockscreen progress bar, optional
+
+  // Android only, optional
+  // text displayed in the status bar when the notification (and the ticker) are updated, optional
+  ticker    : 'Now playing "Time is Running Out"',
+  // All icons default to their built-in android equivalents
+ // The supplied drawable name, e.g. 'media_play', is the name of a drawable found under android/res/drawable* folders
+  playIcon: 'media_play',
+  pauseIcon: 'media_pause',
+  prevIcon: 'media_prev',
+  nextIcon: 'media_next',
+  closeIcon: 'media_close',
+  notificationIcon: 'notification'
+ });
+
+ this.musicControls.subscribe().subscribe(action => {
+
+   function events(action) {
+     const message = JSON.parse(action).message;
+         switch(message) {
+             case 'music-controls-next':
+                 // Do something
+                 break;
+             case 'music-controls-previous':
+                 // Do something
+                 break;
+             case 'music-controls-pause':
+                 // Do something
+                 break;
+             case 'music-controls-play':
+                 // Do something
+                 break;
+             case 'music-controls-destroy':
+                 // Do something
+                 break;
+
+         // External controls (iOS only)
+         case 'music-controls-toggle-play-pause' :
+                 // Do something
+                 break;
+         case 'music-controls-seek-to':
+           const seekToInSeconds = JSON.parse(action).position;
+           this.musicControls.updateElapsed({
+             elapsed: seekToInSeconds,
+             isPlaying: true
+           });
+           // Do something
+           break;
+         case 'music-controls-skip-forward':
+           // Do something
+           break;
+         case 'music-controls-skip-backward':
+           // Do something
+           break;
+
+             // Headset events (Android only)
+             // All media button events are listed below
+             case 'music-controls-media-button' :
+                 // Do something
+                 break;
+             case 'music-controls-headset-unplugged':
+                 // Do something
+                 break;
+             case 'music-controls-headset-plugged':
+                 // Do something
+                 break;
+             default:
+                 break;
+         }
+     }
+
+ this.musicControls.listen(); // activates the observable above
+
+ this.musicControls.updateIsPlaying(true);
+			
+			
+
+			
+			}
+		
+		 )};
 			}
 	
-	}
-	
+}
 }
