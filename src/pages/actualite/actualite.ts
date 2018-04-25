a6c79e8679c531807ff561d0ffa2d712b1c67280
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-04-25 10:34:56 +0200

    fix super tabs

--- src/pages/actualite/actualite.ts
+++ src/pages/actualite/actualite.ts
@@ -1,9 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, IonicPageModule, NavController, NavParams } from 'ionic-angular';
 
-import { SuperTabsController } from '../../ionic2-super-tabs/src';
-import {SuperTabs} from "../../ionic2-super-tabs/src/components/super-tabs";
-
 import { BlogPage } from '../../pages/blog/blog';
 
 @Component({
@@ -11,31 +8,11 @@ import { BlogPage } from '../../pages/blog/blog';
   templateUrl: 'actualite.html'
 })
 export class ActualitePage {
-	  page1: any = BlogPage;
+	 
 	
-	  @ViewChild(SuperTabs) superTabs: SuperTabs;
-
-   showIcons: boolean = false;
-  showTitles: boolean = true;
-  pageTitle: string = 'Full Height';
 	
-  selectedTab = 0;
+  constructor(public navCtrl: NavController, private navParams: NavParams) {
  
-	
-	
-  constructor(public navCtrl: NavController, private navParams: NavParams, private superTabsCtrl: SuperTabsController) {
-    const type = navParams.get('type');
-    switch (type) {
-      case 'icons-only':
-        this.showTitles = false;
-        this.pageTitle += ' - Icons only';
-        break;
-
-      case 'titles-only':
-        this.showIcons = false;
-        this.pageTitle += ' - Titles only';
-        break;
-    }
   }
 
   ngAfterViewInit() {
@@ -49,8 +26,6 @@ export class ActualitePage {
     // }, 2000);
   }
 
-  onTabSelect(tab: { index: number; id: string; }) {
-    console.log(`Selected tab: `, tab);
-  }
+ 
 
 }
