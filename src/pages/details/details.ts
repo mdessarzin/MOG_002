425e7698cfcde8e6a56484a0a78d3c722165bb87
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-06-13 08:57:04 +0200

    f

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -66,6 +66,7 @@ link: string;
 private resize(){
 	var iframe = $('#remotedata');
 		$('#remotedata').height(iframe[0].contentWindow.document.body.scrollHeight + 40);
+	alert('ok');
 }	
 
   ionViewDidLoad() {
