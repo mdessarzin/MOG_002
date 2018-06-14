deb276ee6d4d8434c0ab8bf7d9a36d18f766fbe9
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-06-14 09:49:31 +0200

    f

--- src/pages/podcasts/podcasts.ts
+++ src/pages/podcasts/podcasts.ts
@@ -121,7 +121,7 @@ update(refresher) {
 	  localStorage.setItem("player", "stop");
 	 this._player.pauseProvider();
 	 this._player.playerconfigProvider(link);
-	 this.startAudio(title,image, text, date, link);
+	 //this.startAudio(title,image, text, date, link);
 	 $('ion-footer').animate({'margin-bottom': '0px' }, 200);
 	$('.songArtist').html(text);
 	$('.songTitle').html(title);
