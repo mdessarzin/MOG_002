898a4fa6a9a92382f0c1951215fcb6203bcb5bbb
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-06-14 10:26:46 +0200

    f

--- src/pages/player/player.ts
+++ src/pages/player/player.ts
@@ -118,15 +118,16 @@ typeplayer: any;
 			this._player.stream.getCurrentPosition().then((curpos) => {
 				console.log(curpos);
 				this.positions = curpos;
-				if(this._player.stream.getDuration()=='-1'){
-					this.durations = this._player.stream.getDuration();  
-				}
+
 			});					
 			
 			this.timingseek = setInterval(() => {      
 				this._player.stream.getCurrentPosition().then((curpos) => {
 					console.log(curpos);
 					this.positions = curpos;
+									if(this._player.stream.getDuration()=='-1'){
+					this.durations = this._player.stream.getDuration();  
+				}
 				});					
 			}, 1000);
 
