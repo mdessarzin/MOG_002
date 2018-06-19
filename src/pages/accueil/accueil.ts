30197e11ec611bae1c2b8f86293b6b929c24e8da
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-06-19 16:54:11 +0200

    f

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -115,14 +115,7 @@ update(refresher) {
 
 ionViewDidLoad() {
 
-	var sas_tmstp=Math.round(Math.random()*10000000000);sas_masterflag=1;
-function sasmobile(sas_pageid,sas_formatid,sas_target) {
- if (sas_masterflag==1) {sas_masterflag=0;sas_master='M';} else {sas_master='S';};
- document.write('<scr'+'ipt src="https://www3.smartadserver.com/call2/pubmj/'+sas_pageid+'/'+sas_formatid+'/'+sas_master+'/'+sas_tmstp+'/'+escape(sas_target)+'?"></scr'+'ipt>');
-}function sascc(sas_imageid,sas_pageid) {
-img=new Image();
-img.src='https://www3.smartadserver.com/h/mcp?imgid='+sas_imageid+'&pgid='+sas_pageid+'&uid=[uid]&tmstp='+sas_tmstp+'&tgt=[targeting]';
-}
+
 	
 	
 	  if(localStorage.type_player == 'live'){
