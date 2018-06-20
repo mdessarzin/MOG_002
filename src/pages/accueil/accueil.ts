f90c98c6f9644907db8d722829fc0e57013b4045
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-06-20 16:35:48 +0200

    f

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -46,7 +46,23 @@ export class AccueilPage {
 	pagination: number = 1;
 	maximumPages = 10;
 	posts: Array<any> = [];
-
+options : InAppBrowserOptions = {
+    location : 'yes',//Or 'no' 
+    hidden : 'no', //Or  'yes'
+    clearcache : 'yes',
+    clearsessioncache : 'yes',
+    zoom : 'yes',//Android only ,shows browser zoom controls 
+    hardwareback : 'yes',
+    mediaPlaybackRequiresUserAction : 'no',
+    shouldPauseOnSuspend : 'no', //Android only 
+    closebuttoncaption : 'Close', //iOS only
+    disallowoverscroll : 'no', //iOS only 
+    toolbar : 'yes', //iOS only 
+    enableViewportScale : 'no', //iOS only 
+    allowInlineMediaPlayback : 'no',//iOS only 
+    presentationstyle : 'pagesheet',//iOS only 
+    fullscreen : 'yes',//Windows only    
+};
   constructor(
 		public navCtrl: NavController,
 		public _player: AudioStreamProvider,
