7628c2f7093a799b120669b3176cbf5d447ed631
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-06-29 10:15:15 +0200

    f

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -102,7 +102,9 @@ update(refresher) {
 			 (<any>window).SmartAdServer.createBanner( {
 				adId: '947330/33546', 
 				autoShow: true,
-				 adSize: 'SMART_BANNER'
+				 adSize: 'CUSTOM',
+  width: 640,	// banner width, if set adSize to 'CUSTOM'
+    height: 100	// banner height, if set adSize to 'CUSTOM'
 			  });
 
 	  }	
