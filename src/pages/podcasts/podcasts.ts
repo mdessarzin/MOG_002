bcdbb7996efdf8dcdf3e9f40f3f0c06efeb4e9fd
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-07-04 14:47:15 +0200

    fix podcast

--- src/pages/podcasts/podcasts.ts
+++ src/pages/podcasts/podcasts.ts
@@ -156,30 +156,7 @@ ionViewDidLoad() {
 			$('.playerEtat_0').show();
         }
 	
-	this.plt.ready().then(() => {
-				
-				//Leaderboard
-				let ratio = Math.max(window.devicePixelRatio || 1, 1);
 
-				(<any>window).SmartAdServer.setOptions({
-					siteId: 241727,
-					baseUrl: 'http://mobile.smartadserver.com',
-					position: (<any>window).SmartAdServer.AD_POSITION.BOTTOM_CENTER,
-						// offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
-					bgColor: 'black', // color name, or '#RRGGBB'
-					isTesting: false, // set to true, to receiving test ad for testing purpose
-					autoShow: true, // auto show interstitial ad when loaded, set to false if prepare/show
-				});
-				
-				
-				(<any>window).SmartAdServer.createBanner( {
-					adId: '947330/33546', 
-					autoShow: true,
-					width: this.plt.width()*ratio,
-					height: (this.plt.width()*0.15625)*ratio
-				});	
-				console.log('Platform is ready');
-			});	 
 	
 }
 	
