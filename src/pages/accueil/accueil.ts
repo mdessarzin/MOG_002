eae5c12062d083a622482f917c2f71484d790f3f
Matthieu Dessarzin <mdessarzin@mediaonegroup.ch>
2018-07-05 10:34:51 +0200

    f

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -25,7 +25,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
     trigger('fadeInOut', [
       state('void', style({ opacity: '0' })),
       state('*', style({ opacity: '1' })),
-      transition('void <=> *', animate('500ms ease-in'))
+      transition('void <=> *', animate('1000ms ease-in'))
     ])
   ]
 })
